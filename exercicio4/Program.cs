1b9205e2222e24bd307f1a5508a6a0388e5abbf9
LORENZO MANGILE FARIA <53101823811@info.local>
2025-10-28 08:56:20 -0300

    Atualizando a pasta

--- exercicio4/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿   Console.Write("Digite a senha: ");
-        int senha = int.Parse(Console.ReadLine());
-
-        if (senha == 134)
-            Console.WriteLine("Acesso permitido");
-        else
-            Console.WriteLine("Acesso negado");
